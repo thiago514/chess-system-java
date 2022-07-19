8f98e3dda890c58ecb58b85587920b25b6aeee4f
Thiago Gogola Milagres <thiagogogola9@gmail.com>
2022-07-19 15:03:16 -0300

    First class: Possition

---
+++ src/application/program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class program {
+
+	public static void main(String[] args) {
+		Position pos = new Position(3,5);
+		System.out.println(pos);
+
+	}
+
+}
